4e1bf80b50884673c0f50d55a5e638191a530824
abhishek kaushal <abhikaushalak17@gmail.com>
2023-12-08 17:05:36 +0530

    5:35

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-    function toggleMainContent() {
-      $('main').toggleClass('shifted');
-    }
-    new DataTable('#example');
-    
+function toggleMainContent() {
+  $('main').toggleClass('shifted');
+}
+$(document).ready(function () {
+  // Open sidebar by default on large screens
+  if (window.innerWidth >= 992) {
+    $('.sidebar').addClass('show'); 
+    toggleMainContent(); 
+  }
+
+  // Toggle sidebar on button click
+  // $('#sidebarToggle').click(function () {
+  //   $('.sidebar').toggleClass('show');
+  //   toggleMainContent(); // Apply the main content toggle function
+  // });
+});
+
+
+new DataTable('#example');
+
 
