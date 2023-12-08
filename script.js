6a516e3209c3a78b9cc61e6a4ba3c45036fa8066
abhishek kaushal <abhikaushalak17@gmail.com>
2023-12-08 13:21:52 +0530

    added button features

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-    function toggleMainContent() {
-      $('main').toggleClass('shifted');
-    }
+    
+    // toggle button functionality
+    document.addEventListener('DOMContentLoaded', function () {
+      const toggleBtn = document.getElementById('toggle-btn');
+      const sidebar = document.getElementById('sidebar');
+    
+      toggleBtn.addEventListener('click', function () {
+        sidebar.classList.toggle('open');
+      });
+    
+      // Open sidebar by default on large screens
+      if (window.innerWidth >= 993) {
+        sidebar.classList.add('open');
+      }
+    });
+
+    
+    
+    // Data table
+    
     new DataTable('#example');
     
 
